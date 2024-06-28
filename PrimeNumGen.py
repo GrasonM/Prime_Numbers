ae3741e633deb2bba24516f3fd85afa717476f79
GrasonM <53267706+GrasonM@users.noreply.github.com>
2024-06-28 14:48:30 -0400

    Add files via upload

---
+++ PrimeNumGen.py
@@ -0,0 +1,59 @@
+import unittest
+
+#Written and edited in Jupyter Notebook by Grason Moye
+
+# returns true or false for prime numbers within provided range 
+def prime(value: int) -> bool:
+    #1 is neither prime nor composite
+    if value <= 1:
+        return False
+    #2 is the smallest prime number and the only even prime number
+    if value == 2:
+        return True
+    #Zero is neither prime nor composite
+    if value % 2 == 0:
+        return False
+    #loops through the range of a provided number and checks for factors from 3 to the square root of the number
+    for i in range(3, int(value**0.5) + 1, 2):
+        #Zero is neither prime nor composite
+        if value % i == 0:
+            return False
+    return True
+
+# Prime number generator function - lists prime numbers between input integers
+def prime_generator(start: int, end: int) -> list[int]:
+    if start > end:
+        #input correction if 1st number is greater than 2nd number
+        start, end = end, start
+    return [number for number in range(start, end + 1) if prime(number)]
+
+# Command-line interface simulation
+def main(start: int, end: int):
+    primes = prime_generator(start, end)
+    print(f"Prime numbers between {start} and {end}: {primes}")
+
+#Class that tests functions
+class TestFunctions(unittest.TestCase):
+
+    #tests prime boolean functionality
+    def test_is_prime(self):
+        self.assertFalse(prime(0))
+        self.assertFalse(prime(1))
+        self.assertTrue(prime(2))
+        self.assertTrue(prime(3))
+        self.assertFalse(prime(4))
+        self.assertTrue(prime(5))
+        self.assertFalse(prime(9))
+        self.assertTrue(prime(29))
+
+    #prime number generator output validity check
+    def test_prime_generator(self):
+        self.assertEqual(prime_generator(1, 10), [2, 3, 5, 7])
+        self.assertEqual(prime_generator(10, 1), [2, 3, 5, 7])
+        self.assertEqual(prime_generator(7900, 7920), [7901, 7907, 7919])
+
+# Running the tests
+unittest.main(argv=[''], verbosity=2, exit=False)
+
+# Run the main function and input with a specific range (example: 1 {Enter Key}, 10 {Enter Key})
+main(int(input("Input starting number: ")), int(input("Input ending number: ")))
